1caf68133de428b59b901b12b9241f5f97582765
Louiso <Gransullca.25@gmail.com>
2017-03-27 09:12:04 -0500

    Se agrego un slider, pronto con imagenes propias

---
+++ public/js/init.js
@@ -0,0 +1,9 @@
+//  $(document).ready(function(){
+//           $('.slider').slider();
+// });
+(function($){
+  $(function(){
+    // $('.carousel').carousel();
+    $('.slider').slider();
+  }); // end of document ready
+})(jQuery);
